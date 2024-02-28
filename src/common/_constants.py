f9aba6581f1a030c20dbfdc6cb369808b3c102fe
Kkkasum <kasum2605@mail.ru>
2024-02-28 17:50:22 +0400

    fixed tests

--- src/common/_constants.py
+++ src/common/_constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 LOGS_DIR = BASE_DIR / 'logs'
--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,4 +1,15 @@
+from aiogram import Dispatcher
+
 from ._catalog import router as catalog_router
 from ._menu import router as menu_router
 from ._payment import router as payment_router
 from ._profile import router as profile_router
+
+
+def include_routers(dp: Dispatcher) -> None:
+    dp.include_routers(
+        menu_router,
+        profile_router,
+        payment_router,
+        catalog_router
+    )
